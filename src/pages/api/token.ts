6d8340dc5380433d22f3b241018746006cb07ea1
xuzhihao.7749 <xuzhihao.7749@bytedance.com>
2023-09-28 11:13:54 +0800

    feat: save

---
+++ src/pages/api/token.ts
@@ -0,0 +1,20 @@
+// import { get } from "@vercel/edge-config";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type ResponseData = {
+  message: string;
+  challenge?: any;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseData>
+) {
+  try {
+    const body = JSON.parse(req.body);
+    // const greeting = await get("greeting");
+    res.status(200).json({ message: "success", challenge: body.challenge });
+  } catch (error) {
+    res.status(200).json({ message: "failed" });
+  }
+}
